85f6730d20b226438605d863b2fd4450406884bc
Nico Verzijp <nico.verzijp@nokia.com>
2018-03-17 14:54:26 +0100

    do not connect socket in dev mode

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,12 @@
 import io from 'socket.io-client'
 
+let autoConnect = true
+if (process.env.VUE_APP_SOCKET === 'false') {
+  autoConnect = false
+}
+
 const socket = io({
+  autoConnect,
   path: '/api',
 })
 
