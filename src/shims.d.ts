2ec7e4763f12684a0ed507fcc4e799bd68eef0c8
Nico Verzijp <nico.verzijp@nokia.com>
2018-03-31 11:07:28 +0200

    use ts-vue-plugin to get type support in unit tests

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,7 +1,12 @@
+// Don't need this anymore with ts-vue-plugin plugin
+//   https://github.com/vuejs/vetur/issues/720
+// May be intergrated into vetur at some point?
+/*
 declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
 }
+*/
 
 // https://github.com/FortAwesome/vue-fontawesome/issues/24
 declare module '@fortawesome/*'
