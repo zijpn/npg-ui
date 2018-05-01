05e5a7172e547021ef7ba7de4ba83ca6f5973236
Nico Verzijp <nverzijp@gmail.com>
2018-05-01 13:15:56 +0200

    upgrade to vue-cli beta9

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ module.exports = {
   ],
   transform: {
     '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
     '^.+\\.tsx?$': 'ts-jest'
   },
   moduleNameMapper: {
@@ -16,5 +17,8 @@ module.exports = {
   },
   snapshotSerializers: [
     'jest-serializer-vue'
+  ],
+  testMatch: [
+    '<rootDir>/(tests/unit/**/*.spec.(ts|tsx|js)|**/__tests__/*.(ts|tsx|js))'
   ]
 }
