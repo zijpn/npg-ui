720cc052d43fe9b8f68b025a92af9a5cbd18215a
Nico Verzijp <nverzijp@gmail.com>
2018-05-27 23:57:35 +0200

    Terminal component

--- src/components/Term.vue
+++ src/components/Term.vue
@@ -1,23 +1,193 @@
 <template>
-  <div ref="xterm" class="term">
-    <resize-observer @notify="resize"/>
+  <div class="term">
+     <div class="termselect">
+      <select v-model="selected">
+        <option v-for="term in terms" :key="term.id" :value="term.id">
+          {{ term.id }}:{{ term.process }}
+        </option>
+      </select>
+    </div>
+    <div class="kill" @click="killTerm()" v-tooltip.bottom="{ content: 'Kill Terminal', delay: { show: 1500 } }">
+      <font-awesome-icon :icon="trashIcon" size="xs" />
+    </div>
+    <div class="new" @click="newTerm()" v-tooltip.bottom="{ content: 'New Terminal', delay: { show: 1500 } }">
+      <font-awesome-icon :icon="plusIcon" size="xs" />
+    </div>
+    <div ref="xterm" class="inner">
+      <resize-observer @notify="resize"/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
+import faPlus from '@fortawesome/fontawesome-free-solid/faPlus'
+import faTrashAlt from '@fortawesome/fontawesome-free-solid/faTrashAlt'
 import { Component, Provide, Vue } from 'vue-property-decorator'
 
 @Component
 export default class Term extends Vue {
-  @Provide()
-  public term: any
 
-  public fit() {
-    const parent = this.term.element.parentElement as Element
+  @Provide()
+  public terms: Array<{ term: any, id: number, process: string }> = []
+
+  @Provide()
+  public selected: number = 0
+
+  private sock: any = null
+
+  get plusIcon() {
+    return faPlus
+  }
+
+  get trashIcon() {
+    return faTrashAlt
+  }
+
+  public killTerm() {
+    const current = this.findTerm(this.selected)
+    if (!current) {
+      return
+    }
+    if (this.connected()) {
+      this.sock.emit('destroy', {
+        id: current.id,
+      })
+    }
+    current.term.dispose()
+    this.terms = this.terms.filter((term) => term.id !== current.id)
+    this.selected = (this.terms[0]) ? this.terms[0].id : 0
+  }
+
+  public newTerm() {
+    import(/* webpackChunkName: "term" */ 'xterm').then(({Terminal}) => {
+      if (this.connected()) {
+        const term = new Terminal({
+          cursorBlink: true,
+          fontSize: 12,
+          scrollback: 2000,
+        })
+        term.setOption('theme', {
+          background: '#1e1e1e'
+        })
+        this.sock.emit('create', {
+          args: [],
+          cols: term.cols,
+          container: '',
+          rows: term.rows,
+        })
+        term.open(this.$refs.xterm as HTMLElement)
+        // add object to terms array & wait for created msg
+        this.terms.unshift({ term, id: 0, process: '' })
+        this.selected = 0
+      }
+    })
+  }
+
+  public resize() {
+    const current = this.findTerm(this.selected)
+    if (!current) {
+      return
+    }
+    this.fit(current.term)
+    current.term.focus()
+  }
+
+  public mounted() {
+    import(/* webpackChunkName: "sock" */ 'socket.io-client').then((io) => {
+      if (process.env.VUE_APP_SOCKET) {
+        this.sock = io.default('/term', {
+          path: '/api',
+          transports: ['websocket'],
+        })
+        this.sock.on('connect', () => {
+          this.sock.on('created', (msg: { container: string, id: number, process: string }) => {
+            // console.log('terminal created', msg)
+            this.terms[0].id = msg.id
+            this.terms[0].process = msg.process
+            this.fit(this.terms[0].term)
+            this.terms[0].term.focus()
+            this.terms[0].term.on('data', this.sendData)
+            this.terms[0].term.on('resize', this.logResize)
+            this.selected = msg.id
+          })
+          this.sock.on('close', (msg: { id: number }) => {
+            if (msg.id !== this.selected) {
+              // should not happen
+              return
+            }
+            const current = this.findTerm(this.selected)
+            if (current) {
+              this.killTerm()
+            }
+          })
+          this.sock.on('data', (msg: { data: string, id: number }) => {
+            if (msg.id !== this.selected) {
+              // should not happen
+              return
+            }
+            const current = this.findTerm(this.selected)
+            if (current) {
+              current.term.write(msg.data)
+            }
+          })
+        })
+      }
+    })
+  }
+
+  public beforeDestroy() {
+    this.terms.forEach((term) => {
+      term.term.dispose()
+      if (this.connected()) {
+        this.sock.emit('destroy', {
+          id: term.id,
+        })
+      }
+    })
+  }
+
+  public clear() {
+    // this.term.clear()
+  }
+
+  public scrollToBottom() {
+    // this.term.scrollToBottom()
+  }
+
+  private sendData(data: string) {
+    const current = this.findTerm(this.selected)
+    if (!current) {
+      return
+    }
+    if (this.connected()) {
+      this.sock.emit('data', {
+        data,
+        id: current.id,
+      })
+    }
+  }
+
+  private logResize(size: any) {
+    // console.log('Resized to ' + size.cols + ' cols and ' + size.rows + ' rows.')
+    const current = this.findTerm(this.selected)
+    if (!current) {
+      return
+    }
+    if (this.connected()) {
+      this.sock.emit('resize', {
+        cols: size.cols,
+        id: current.id,
+        rows: size.rows,
+      })
+    }
+  }
+
+  private fit(term: any) {
+    const parent = term.element.parentElement as Element
     const parentElementStyle = window.getComputedStyle(parent)
     const parentElementHeight = parseInt(parentElementStyle.getPropertyValue('height'), 10)
     const parentElementWidth = Math.max(0, parseInt(parentElementStyle.getPropertyValue('width'), 10))
-    const elementStyle = window.getComputedStyle(this.term.element)
+    const elementStyle = window.getComputedStyle(term.element)
     const elementPadding = {
       bottom: parseInt(elementStyle.getPropertyValue('padding-bottom'), 10),
       left: parseInt(elementStyle.getPropertyValue('padding-left'), 10),
@@ -27,40 +197,25 @@ export default class Term extends Vue {
     const elementPaddingVer = elementPadding.top + elementPadding.bottom
     const elementPaddingHor = elementPadding.right + elementPadding.left
     const availableHeight = parentElementHeight - elementPaddingVer
-    const scrollBarWidth = (this.term as any).viewport.scrollBarWidth
+    const scrollBarWidth = term.viewport.scrollBarWidth
     const availableWidth = parentElementWidth - elementPaddingHor - scrollBarWidth
-    const renderer = (this.term as any).renderer
+    const renderer = term.renderer
     if (renderer.dimensions.actualCellWidth && renderer.dimensions.actualCellWidth) {
       const cols = Math.floor(availableWidth / renderer.dimensions.actualCellWidth)
       const rows = Math.floor(availableHeight / renderer.dimensions.actualCellHeight)
-      if (this.term.rows !== rows || this.term.cols !== cols) {
+      if (term.rows !== rows || term.cols !== cols) {
         renderer.clear()
-        this.term.resize(cols, rows)
+        term.resize(cols, rows)
       }
     }
   }
 
-  public resize() {
-    const renderer = (this.term as any).renderer
-    if (!renderer) {
-      this.term.open(this.$refs.xterm as HTMLElement)
-    }
-    this.fit()
-    this.term.focus()
+  private findTerm(id: number) {
+    return this.terms.find((term) => term.id === id)
   }
 
-  public mounted() {
-    import(/* webpackChunkName: "term" */ 'xterm').then(({Terminal}) => {
-      this.term = new Terminal({
-        cursorBlink: true,
-        scrollback: 2000,
-      })
-      this.term.on('resize', this.logResize)
-    })
-  }
-
-  private logResize(size: any) {
-    // console.log('Resized to ' + size.cols + ' cols and ' + size.rows + ' rows.')
+  private connected() {
+    return this.sock && this.sock.connected
   }
 }
 </script>
@@ -70,7 +225,40 @@ export default class Term extends Vue {
 </style>
 
 <style lang="scss" scoped>
+.termselect {
+  position: absolute;
+  top: 2px;
+  right: 78px;
+  height: 24px;
+}
+.termselect select {
+  font-size: 0.8em;
+  background-color: rgb(60, 60, 60);
+  color: rgb(240, 240, 240);
+  border-color: rgb(60, 60, 60);
+  border: 0px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+}
+.termselect select:active, .termselect select:focus {
+  outline-style: none;
+}
 .term {
-  flex: 1;
+  height: 100%;
+}
+.inner {
+  height: 100%;
+}
+.kill {
+  position: absolute;
+  top: 2px;
+  right: 30px;
+  cursor: pointer;
+}
+.new {
+  position: absolute;
+  top: 2px;
+  right: 52px;
+  cursor: pointer;
 }
 </style>
